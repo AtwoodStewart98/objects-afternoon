94768b68220906e05761d024ba638a4a82bcb9d5
AtwoodStewart98 <30844246+AtwoodStewart98@users.noreply.github.com>
2017-08-28 17:11:10 -0500

    Add files via upload

--- objects.js
+++ objects.js
@@ -1,64 +1,67 @@
 // Create an object called user that has name, age, password properties and assign them appropriate values.
 
-// Code Here
-
-
+var user = {
+  name: 'Painis Cupcake',
+  age: 83,
+  password: 'Iwill347you'
+}
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
-// Code Here
-
-
+user.email = 'painisweak@teamfortress2.com';
+user.username = 'Soldier76';
+user.alertUser = function() {alert(user.name)};
 
 // Now loop over your user object and alert each of the keys.
 
-// Code Here
-
-
+for (var key in user) {
+  alert(key);
+}
 
 // Now loop over your user object and alert each of the values for each key.
 
-// Code Here
-
-
+for (var key in user) {
+  alert(user[key]);
+}
 
 // Remove the username from your user object
 
-// Code Here
-
-
+user.username = "";
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
-// Code Here
-
-
+var creditCard = {
+  number: 212954759,
+  cvc: 232,
+  type: 'Visa',
+  limit: '$200'
+}
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
-// Code Here
-
-
+var alternating = 'bonusCategories';
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
-    Set it's value to an array of 3 items that are objects. 
-    Each object should have one key value pair. 
+    Set its value to an array of 3 items that are objects.
+    Each object should have one key value pair.
     That key value pair should be the category as the key and the percent it's work for the bonus as the value.
-    (e.g. Department Stores, 5%) 
-    
+    (e.g. Department Stores, 5%)
+
     Using dot notation add another key to your object called 'bank' with the value being an object with 2 keys,
         the bank name, and the bank address.
-    
+
     */
 
-// Code Here
-
-
+creditCard[alternating] = [{category: 'Dank Memes', percent: '42%'}, {category: 'Comic Book Stores', percent: '8%'}, {category: 'Ice Cream Shops', percent: '5%'}];
+creditCard.bank = {bankName: 'Chase', bankAddress: '9023 Broadway Blvd'};
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
-// Code Here
+function yolo(creditCard, user) {
+  for (var key in obj) {
 
+  }
+}
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
     It will also change alive to 'More Than Ever'.
@@ -70,6 +73,12 @@ var obj = {
     alive: 'Sort of',
   };
 
-  // Code Here
-
-  
+  function loopy(obj) {
+    for (var i=0; i<obj.allies.length; i++) {
+      if (obj.allies[i] === 'LittleFinger') {
+        obj.allies[i] = 'Dragon Lady';
+        obj.alive = 'More Than Ever';
+      }
+    }
+    return obj;
+  }
